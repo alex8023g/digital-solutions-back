9c96420a1420957e827e2b9c002022068dd197ca
alex8023 <alex8023@yandex.ru>
2025-12-03 10:06:29 +0300

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { createRecords } from './createRecords';
 import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 let records = createRecords();
 
